f342ae495586c1412b4fe12fb30ec8a62cd56cc9
nainshicoder <66nainshigupta2000@gmail.com>
2026-02-05 23:59:12 +0530

    Update shayari-data.js

--- shayari-data.js
+++ shayari-data.js
@@ -255,67 +255,6 @@ const shayariData = [
  text: "Zindagi ek kahani hai,\nAur hum uske kirdaar."
   },
 
-)
-  { category: "motivation", title: "Himmat", text: "Girte hain sheh-sawaar hi maidaan-e-jung mein,\nWoh tifl kya girenge jo ghutnon ke bal chale." },
-  { category: "motivation", title: "Koshish", text: "Manzilein unhi ko milti hain,\nJinke sapnon mein jaan hoti hai." },
-  { category: "motivation", title: "Umeed", text: "Andhera chhatega hi,\nEk din roshni zaroor aayegi." },
-  { category: "motivation", title: "Jeet", text: "Haar ka dar hai to jeet ka maza nahi,\nDarr se nikal kar dekho, zindagi nayi hai." },
-  { category: "motivation", title: "Kadam", text: "Ek kadam se nahi hoti manzil payi,\nRukna bhi padta hai raah mein kabhi." },
-  { category: "motivation", title: "Sapna", text: "Sapne wahi poore hote hain,\nJinhe dekhne ki aadat ho." },
-  { category: "motivation", title: "Mushkil", text: "Mushkilein aayengi tootne ko,\nPar hum toote nahi, sirf jhuke hain." },
-  { category: "motivation", title: "Waqt", text: "Waqt badalta hai sab kuch,\nBas tum badalne se mat daro." },
-  { category: "motivation", title: "Hausla", text: "Hausla hai to har mushkil aasan,\nBas yakeen ki kami nahi honi chahiye." },
-  { category: "motivation", title: "Rasta", text: "Rasta khud banta hai chalne se,\nTham jaoge to raah bhatak jaogi." },
-
-  // Category: Life (165 shayari)
-  { category: "life", title: "Zindagi", text: "Zindagi ek safar hai suhana,\nYahan kal kya ho kisi ne na jaana." },
-  { category: "life", title: "Pal", text: "Pal do pal ka hi to hai yeh jeevan,\nHar pal ko khushi se jeena seekho." },
-  { category: "life", title: "Yaadein", text: "Yaadein wahi meethi lagti hain,\nJo door chali gayi hain." },
-  { category: "life", title: "Aaj", text: "Kal ki soch mein aaj kho mat do,\nAaj hi toh zindagi hai." },
-  { category: "life", title: "Muskurahat", text: "Muskurahat ek aisi dua hai,\nJo har gham ko halka kar deti hai." },
-  { category: "life", title: "Rishte", text: "Rishte dhoop-chhaon ki tarah hain,\nKabhi garam, kabhi thand mehsus hote hain." },
-  { category: "life", title: "Guzra Waqt", text: "Guzra waqt laut kar nahi aata,\nUse yaadon mein hi saja kar rakhna." },
-  { category: "life", title: "Khushi", text: "Khushi chhoti chhoti cheezon mein hai,\nBadi cheezein to bas dikhavi hoti hain." },
-  { category: "life", title: "Dard", text: "Dard bhi zindagi ka hissa hai,\nUse chhupane ki bajaye samjho." },
-  { category: "life", title: "Badlav", text: "Zindagi badal rahi hai har pal,\nTum bhi badlo, piche mat raho." },
-
-  // Category: Friends (165 shayari)
-  { category: "friends", title: "Dosti", text: "Dosti khoon se nahi, dil se hoti hai,\nSamay ke saath aur gehri ho jati hai." },
-  { category: "friends", title: "Saccha Dost", text: "Saccha dost woh hai,\nJo buri aadat se bhi pyaar se samjhaaye." },
-  { category: "friends", title: "Saath", text: "Door hoke bhi paas rehna hai,\nYahi toh dosti ka asli faisla hai." },
-  { category: "friends", title: "Yaari", text: "Yaari ka rishta aisa hota hai,\nJaise phoolon ki khushboo hawa mein." },
-  { category: "friends", title: "Sahara", text: "Dost woh sahara hai,\nJo har toofan mein saath deta hai." },
-  { category: "friends", title: "Muskurahat", text: "Tumhari muskurahat meri dua hai,\nHar gam mein bhi muskurate raho." },
-  { category: "friends", title: "Samjh", text: "Kaun kehta hai samjhna zaroori hai,\nKabhi kabhi bas saath dena kaafi hota hai." },
-  { category: "friends", title: "Rishta", text: "Dosti ka rishta gehna nahi hota,\nJo pehna jaaye aur utar jaaye." },
-  { category: "friends", title: "Waqt", text: "Waqt ne humein juda kiya hai,\nPar dil aaj bhi ek hi dhadakte hain." },
-  { category: "friends", title: "Yakeen", text: "Dost woh hota hai,\nJise tum apni har baat ka yakeen kar sako." },
-
-  // ... aur 480 shayariyan isi tarah continue hongi
-  // Niche main 20 aur shayariyan complete format mein dikha raha hoon:
-
-  { category: "motivation", title: "Himmat Badhao", text: "Jo dar gaya woh mar gaya,\nJeena hai to darr se ladna seekho." },
-  { category: "life", title: "Anjaan Raaste", text: "Zindagi ke anjaan raaston par,\nKabhi kabhi bhatakna bhi zaroori hai." },
-  { category: "friends", title: "Dil Ki Baat", text: "Dil ki baat wahi samjhta hai,\nJo khud dil se baat karna jaanta hai." },
-  { category: "motivation", title: "Udaan", text: "Udaan bharne do apne sapnon ko,\nParindo ki tarah aazad gagan mein." },
-  { category: "life", title: "Lamhe", text: "Lamhe bikhar jaate hain pani ki tarah,\nPakadna hai to bas yaadon ko pakado." },
-  { category: "friends", title: "Saathi", text: "Zindagi ki raah mein kisi saathi ka hona,\nJaise raat mein chaand ka chamakna." },
-  { category: "motivation", title: "Roshni", text: "Khud hi ban jaao roshni,\nAndhera khud-ba-khud bhag jaayega." },
-  { category: "life", title: "Ibadat", text: "Zindagi jeena hi asli ibadat hai,\nIsse khushiyon se sajao." },
-  { category: "friends", title: "Vishwas", text: "Dosti ka vishwas aisa hota hai,\nJaise jadon ki gehraai." },
-  { category: "motivation", title: "Misaal", text: "Khud ki misaal ban kar dikhao,\nDuniya tumhare piche chalegi." },
-  { category: "life", title: "Rang", text: "Zindagi ke rang hamesha badalte rahenge,\nHar rang mein kho kar dekhna." },
-  { category: "friends", title: "Hamesha", text: "Dosti hamesha ke liye nahi hoti,\nPar jab hoti hai to hamesha ke liye hoti hai." },
-  { category: "motivation", title: "Parwaz", text: "Parwaz ki udaan bhari hai,\nBas thoda sa himmat chahiye." },
-  { category: "life", title: "Safar", text: "Safar mein milenge kayi mod,\nHar mod par nayi kahani likhna." },
-  { category: "friends", title: "Gehrai", text: "Dosti ki gehrai samandar si hai,\nJitna jaoge utna gehrai milegi." },
-  { category: "motivation", title: "Josh", text: "Josh mein jeena seekho,\nDarr ke piche bhagna nahi." },
-  { category: "life", title: "Khwaab", text: "Khwaab dekhte raho,\nWo sach ho jaate hain jo dil se dekhe jaate hain." },
-  { category: "friends", title: "Sabse Khas", text: "Dost wohi hai jo sabse khas ho,\nJiske bina zindagi adhoori si lagti ho." },
-  { category: "motivation", title: "Jeet", text: "Jeet ka jazba dil mein jagao,\nHaar ka khauf dil se nikaalo." },
-  { category: "life", title: "Mauka", text: "Har pal ek naya mauka hai,\nUse kho mat jane do." }
-
-
 ];
 
 /*
